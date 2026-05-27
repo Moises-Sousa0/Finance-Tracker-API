95b1dc04fd4378a241161733276331c5cbb085ba
Moises-Sousa0 <moisessousanow@gmail.com>
2026-05-27 17:54:05 -0300

    criando service.py e autenticacao jwt

--- app/service.py
+++ app/service.py
@@ -1,10 +1,8 @@
 from jose import jwt, JWTError
 from datetime import datetime, timedelta, timezone
 from dotenv import load_dotenv
-from fastapi.security import OAuth2PasswordBearer
-from fastapi import Depends, HTTPException
+from fastapi import HTTPException
 from passlib.context import CryptContext
-from app import models, schemas
 import os
 
 
@@ -12,17 +10,25 @@ load_dotenv()
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORTIHM = os.getenv("ALGORTIHM")
+ALGORITHM = os.getenv("ALGORITHM")
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login") 
 
-def hash_password():
-    hash_senha = pwd_context.hash()
-
+def hash_password(password: str):
+    return pwd_context.hash(password)
+    
 def criar_token(dados: dict):
     payload = dados.copy()
     expiracao = datetime.now(timezone.utc) + timedelta(minutes=30)
     payload.update({"exp": expiracao})
-    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORTIHM)
+    return jwt.encode(payload, SECRET_KEY, algorithm=ALGORITHM)
 
+def verificar_token(token):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(status_code=401, detail="Login invalido")
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Token invalido ou inexistente")
 
+    return user_id
