8e0926e9d73865849840a7d0b7b201ce621b7756
Moises-Sousa0 <moisessousanow@gmail.com>
2026-05-28 00:54:12 -0300

    adicionando get_current_user

---
+++ app/auth/dependencies.py
@@ -0,0 +1,14 @@
+from app.auth.service import verify_token
+from fastapi import Depends
+from fastapi.security import  OAuth2PasswordBearer
+from app.database import get_db
+from sqlalchemy.orm import Session
+from app import models
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    token_verify = verify_token(token)
+    search_user = db.query(models.User).filter(models.User.id == int(token_verify))
