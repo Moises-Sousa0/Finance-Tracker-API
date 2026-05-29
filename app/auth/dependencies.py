e9ce6c8950de31912e3a85451e798437d07a4135
Moises-Sousa0 <moisessousanow@gmail.com>
2026-05-29 09:51:26 -0300

    criando dependencies.py e criando funcao verify_password

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,5 +1,5 @@
 from app.auth.service import verify_token
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from fastapi.security import  OAuth2PasswordBearer
 from app.database import get_db
 from sqlalchemy.orm import Session
@@ -11,4 +11,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
     token_verify = verify_token(token)
-    search_user = db.query(models.User).filter(models.User.id == int(token_verify))
+    search_user = db.query(models.User).filter(models.User.id == int(token_verify)).first()
+    
+    if search_user is None:
+        raise HTTPException(status_code=401, detail="Usuario não encontrado")
+
+    return search_user
--- app/auth/service.py
+++ app/auth/service.py
@@ -35,3 +35,4 @@ def verify_token(token):
 
 def verify_password(plain_password: str, hashed_password: str): 
     return pwd_context.verify(plain_password, hashed_password)
+
